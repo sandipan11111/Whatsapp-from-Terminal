52ddd934d43b7cdad30e636ed67365992c4d670a
Sandipan Dutta <sandipandutta11111.com>
2021-10-30 12:43:14 +0530

    Commit #1

--- whatsapp.py
+++ whatsapp.py
@@ -43,6 +43,7 @@ def send(driver,msg):
 def receive(driver):
 
     print("Hello From Sandipan")
+    print("Hello again")
     incoming_scheduler.enter(5,1,helper,(driver,incoming_scheduler))
     incoming_scheduler.run()
 
